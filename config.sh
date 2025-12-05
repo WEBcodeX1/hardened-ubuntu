689d116f2978338e55afb62012f941dfb12210bb
Claus Prüfer <pruefer@webcodex.de>
2025-12-05 13:44:26 +0100

    Add base configuration file

---
+++ config.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# netplan
+export NET_IF_NAME=enp2s0
+export NET_IF_MACADDRESS=12:34:56:78:9a:9b
+export NET_IF_MTU=9000
+
+# nextdns
+export NEXTDNS_ID="abcdef"
+export NEXTDNS_STAMP=""
+
+# kernel
+export KERNEL_DOMAIN_NAME="domain.name"
+
+# sysusers
+export USER_IDS="ubuntu"
