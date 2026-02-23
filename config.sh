bb128eb4ebc87c8435b1454f00cb2e3d8e5b7f8c
Claus Prüfer <pruefer@webcodex.de>
2026-02-23 13:13:35 +0100

    Improve default property quality

--- config.sh
+++ config.sh
@@ -1,16 +1,21 @@
 #!/bin/sh
 
-# netplan
-export NET_IF_NAME=enp2s0
-export NET_IF_MACADDRESS=12:34:56:78:9a:9b
-export NET_IF_MTU=9000
+# netplan ethernet
+export NET_IF_NAME="enp2s0"
+export NET_IF_MACADDRESS="12:34:56:78:9a:9b"
+export NET_IF_MTU="9000"
+
+# netplan wifi
+export NET_WIFI_IF_NAME="wlp0s20f3"
+export NET_WIFI_AP_SSID="Wifi-SSID"
+export NET_WIFI_AP_PASS="Wifi-Password"
 
 # nextdns
-export NEXTDNS_ID="abcdef"
-export NEXTDNS_STAMP=""
+export NEXTDNS_ID="a1b2c3"
+export NEXTDNS_STAMP="Base64Hash"
 
 # kernel
 export KERNEL_DOMAIN_NAME="domain.name"
 
 # sysusers
-export USER_IDS="ubuntu"
+export USER_IDS="admin user1"
