c78f6df5247e0e00b08c2595799f381355ba7c79
Claus Prüfer <pruefer@webcodex.de>
2026-02-24 12:44:31 +0100

    Prevent appending "Hidden=True" on each user login (multiple times)

--- prepare-user-autostart.sh
+++ prepare-user-autostart.sh
@@ -3,27 +3,38 @@
 # make xdg autostart dir
 mkdir -p ~/.config/autostart
 
-# copy .desktop files
-cp /etc/xdg/autostart/geoclue-demo-agent.desktop ~/.config/autostart/
-cp /etc/xdg/autostart/im-launch.desktop ~/.config/autostart/
-cp /etc/xdg/autostart/orca-autostart.desktop ~/.config/autostart/
-cp /etc/xdg/autostart/org.gnome.Evolution-alarm-notify.desktop ~/.config/autostart/
-cp /etc/xdg/autostart/snap-userd-autostart.desktop ~/.config/autostart/
-cp /etc/xdg/autostart/spice-vdagent.desktop ~/.config/autostart/
-cp /etc/xdg/autostart/ubuntu-advantage-notification.desktop ~/.config/autostart/
-cp /etc/xdg/autostart/ubuntu-report-on-upgrade.desktop ~/.config/autostart/
-cp /etc/xdg/autostart/update-notifier.desktop ~/.config/autostart/
+# copy .desktop files to /tmp
+cp /etc/xdg/autostart/geoclue-demo-agent.desktop /tmp/
+cp /etc/xdg/autostart/im-launch.desktop /tmp/
+cp /etc/xdg/autostart/orca-autostart.desktop /tmp/
+cp /etc/xdg/autostart/org.gnome.Evolution-alarm-notify.desktop /tmp/
+cp /etc/xdg/autostart/snap-userd-autostart.desktop /tmp/
+cp /etc/xdg/autostart/spice-vdagent.desktop /tmp/
+cp /etc/xdg/autostart/ubuntu-advantage-notification.desktop /tmp/
+cp /etc/xdg/autostart/ubuntu-report-on-upgrade.desktop /tmp/
+cp /etc/xdg/autostart/update-notifier.desktop /tmp/
 
 # disable autostart
-echo "Hidden=true" >> ~/.config/autostart/geoclue-demo-agent.desktop
-echo "Hidden=true" >> ~/.config/autostart/im-launch.desktop
-echo "Hidden=true" >> ~/.config/autostart/orca-autostart.desktop
-echo "Hidden=true" >> ~/.config/autostart/org.gnome.Evolution-alarm-notify.desktop
-echo "Hidden=true" >> ~/.config/autostart/snap-userd-autostart.desktop
-echo "Hidden=true" >> ~/.config/autostart/spice-vdagent.desktop
-echo "Hidden=true" >> ~/.config/autostart/ubuntu-advantage-notification.desktop
-echo "Hidden=true" >> ~/.config/autostart/ubuntu-report-on-upgrade.desktop
-echo "Hidden=true" >> ~/.config/autostart/update-notifier.desktop
+echo "Hidden=true" >> /tmp/geoclue-demo-agent.desktop
+echo "Hidden=true" >> /tmp/im-launch.desktop
+echo "Hidden=true" >> /tmp/orca-autostart.desktop
+echo "Hidden=true" >> /tmp/org.gnome.Evolution-alarm-notify.desktop
+echo "Hidden=true" >> /tmp/snap-userd-autostart.desktop
+echo "Hidden=true" >> /tmp/spice-vdagent.desktop
+echo "Hidden=true" >> /tmp/ubuntu-advantage-notification.desktop
+echo "Hidden=true" >> /tmp/ubuntu-report-on-upgrade.desktop
+echo "Hidden=true" >> /tmp/update-notifier.desktop
+
+# install .desktop files
+cp /tmp/geoclue-demo-agent.desktop ~/.config/autostart/
+cp /tmp/im-launch.desktop ~/.config/autostart/
+cp /tmp/orca-autostart.desktop ~/.config/autostart/
+cp /tmp/org.gnome.Evolution-alarm-notify.desktop ~/.config/autostart/
+cp /tmp/snap-userd-autostart.desktop ~/.config/autostart/
+cp /tmp/spice-vdagent.desktop ~/.config/autostart/
+cp /tmp/ubuntu-advantage-notification.desktop ~/.config/autostart/
+cp /tmp/ubuntu-report-on-upgrade.desktop ~/.config/autostart/
+cp /tmp/update-notifier.desktop ~/.config/autostart/
 
 # disable gnome extensions
 desktop_file="$HOME/.config/autostart/disable-gnome-extension-ding.desktop"
