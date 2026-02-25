cd55c9c24e480d52a5fdbce3c47f2f54a357d555
Claus Prüfer <pruefer@webcodex.de>
2026-02-25 12:14:28 +0100

    Lock settings (user can not override)

--- firefox-global.js
+++ firefox-global.js
@@ -11,7 +11,7 @@
 // preferences files if you don't want users to be able to override
 // some preferences.
 
-pref("extensions.update.enabled", false);
+pref("extensions.update.enabled", false, locked);
 
 // Use LANG environment variable to choose locale
 pref("intl.locale.requested", "");
@@ -20,151 +20,151 @@ pref("intl.locale.requested", "");
 pref("browser.shell.checkDefaultBrowser", false);
 
 // Disable openh264.
-pref("media.gmp-gmpopenh264.enabled", false);
+pref("media.gmp-gmpopenh264.enabled", false, locked);
 
 // Default to classic view for about:newtab
 pref("browser.newtabpage.enhanced", false, sticky);
 
 // Disable telemetry and data collection
-pref("datareporting.healthreport.uploadEnabled", false);
-pref("datareporting.policy.dataSubmissionEnabled", false);
-pref("toolkit.telemetry.enabled", false);
+pref("datareporting.healthreport.uploadEnabled", false, locked);
+pref("datareporting.policy.dataSubmissionEnabled", false, locked);
+pref("toolkit.telemetry.enabled", false, locked);
 
 // Default to no suggestions in the urlbar. This still brings a panel asking
 // the user whether they want to opt-in on first use.
-pref("browser.urlbar.suggest.searches", false);
+pref("browser.urlbar.suggest.searches", false, locked);
 
 // Disable HTTP2
-pref("network.http.http2.enabled", false);
+pref("network.http.http2.enabled", false, locked);
 
 // Disable HTTP3
-pref("network.http.http3.enable", false);
+pref("network.http.http3.enable", false, locked);
 
 // Disable Clibboard Events
-pref("dom.event.clipboardevents.enabled", false);
-pref("clipboard.autocopy", false);
+pref("dom.event.clipboardevents.enabled", false, locked);
+pref("clipboard.autocopy", false, locked);
 
 // Disable Accessibility
-pref("accessibility.force_disabled", 1);
+pref("accessibility.force_disabled", 1, locked);
 
 // Disable Service Workers
-pref("dom.serviceWorkers.enabled", false);
+pref("dom.serviceWorkers.enabled", false, locked);
 
 // Disable Normandy
-pref("app.normandy.enabled", false);
+pref("app.normandy.enabled", false, locked);
 
 // Disable Autoupdate
-pref("app.update.auto", false);
+pref("app.update.auto", false, locked);
 
 // Disable Beacon
-pref("beacon.enabled", false);
+pref("beacon.enabled", false, locked);
 
 // Disable Profile Backup
-pref("browser.backup.enabled", false);
+pref("browser.backup.enabled", false, locked);
 
 // Disable Discovery
-pref("browser.discovery.enabled", false);
+pref("browser.discovery.enabled", false, locked);
 
 // Enable Download Always Ask
-pref("browser.download.always_ask_before_handling_new_types", true);
-pref("browser.download.panel.shown", true);
-pref("browser.download.useDownloadDir", false);
+pref("browser.download.always_ask_before_handling_new_types", true, locked);
+pref("browser.download.panel.shown", true, locked);
+pref("browser.download.useDownloadDir", false, locked);
 
 // Disable http to https Auto-Fix URL Input
-pref("browser.fixup.fallback-to-https", false);
+pref("browser.fixup.fallback-to-https", false, locked);
 
 // Disable Formfill
-pref("browser.formfill.enable", false);
+pref("browser.formfill.enable", false, locked);
 
 // Disable AI
-pref("browser.ml.enable", false);
-pref("browser.ml.chat.enabled", false);
+pref("browser.ml.enable", false, locked);
+pref("browser.ml.chat.enabled", false, locked);
 
 // Disable Experimental
-pref("browser.preferences.experimental", false);
+pref("browser.preferences.experimental", false, locked);
 
 // Disable Promo
-pref("browser.promo.focus.enabled", false);
-pref("browser.promo.pin.enabled", false);
-pref("browser.vpn_promo.enabled", false);
-pref("browser.preferences.moreFromMozilla", false);
+pref("browser.promo.focus.enabled", false, locked);
+pref("browser.promo.pin.enabled", false, locked);
+pref("browser.vpn_promo.enabled", false, locked);
+pref("browser.preferences.moreFromMozilla", false, locked);
 
 // Search Settings
-pref("browser.search.suggest.enabled", false);
-pref("browser.search.update", false);
+pref("browser.search.suggest.enabled", false, locked);
+pref("browser.search.update", false, locked);
 
 // Homepage Settings
-pref("browser.startup.homepage.abouthome_cache.enabled", false);
+pref("browser.startup.homepage.abouthome_cache.enabled", false, locked);
 
 // Tab Settings
-pref("browser.tabs.groups.enabled", false);
-pref("browser.tabs.hoverPreview.enabled", false);
+pref("browser.tabs.groups.enabled", false, locked);
+pref("browser.tabs.hoverPreview.enabled", false, locked);
 
 // Translation Settings
-pref("browser.translations.enable", false);
+pref("browser.translations.enable", false, locked);
 
 // Misc Settings
-pref("browser.topsites.contile.enabled", false);
-pref("browser.touchmode.auto", false);
-pref("browser.triple_click_selects_paragraph", false);
-pref("browser.uitour.enabled", false);
+pref("browser.topsites.contile.enabled", false, locked);
+pref("browser.touchmode.auto", false, locked);
+pref("browser.triple_click_selects_paragraph", false, locked);
+pref("browser.uitour.enabled", false, locked);
 
 // URLBar Settings
-pref("browser.urlbar.autoFill", false);
-pref("browser.urlbar.groupLabels.enabled", false);
-pref("browser.urlbar.quicksuggest.enabled", false);
-pref("browser.urlbar.speculativeConnect.enabled", false);
-pref("browser.urlbar.suggest.engines", false);
-pref("browser.urlbar.suggest.quickactions", false);
-pref("browser.browser.urlbar.suggest.recentsearches", false);
-pref("browser.urlbar.suggest.remotetab", false);
-pref("browser.urlbar.suggest.weather", false);
-pref("browser.urlbar.trending.featureGate", false);
+pref("browser.urlbar.autoFill", false, locked);
+pref("browser.urlbar.groupLabels.enabled", false, locked);
+pref("browser.urlbar.quicksuggest.enabled", false, locked);
+pref("browser.urlbar.speculativeConnect.enabled", false, locked);
+pref("browser.urlbar.suggest.engines", false, locked);
+pref("browser.urlbar.suggest.quickactions", false, locked);
+pref("browser.browser.urlbar.suggest.recentsearches", false, locked);
+pref("browser.urlbar.suggest.remotetab", false, locked);
+pref("browser.urlbar.suggest.weather", false, locked);
+pref("browser.urlbar.trending.featureGate", false, locked);
 
 // Datareporting Settings
-pref("datareporting.usage.uploadEnabled", false);
+pref("datareporting.usage.uploadEnabled", false, locked);
 
 // Sensors Settings
-pref("device.sensors.enabled", false);
+pref("device.sensors.enabled", false, locked);
 
 // Devtools Settings
-pref("devtools.accessibility.enabled", false);
-pref("devtools.source-map.client-service.enabled", false);
+pref("devtools.accessibility.enabled", false, locked);
+pref("devtools.source-map.client-service.enabled", false, locked);
 
 // Battery Settings
-pref("dom.battery.enabled", false);
+pref("dom.battery.enabled", false, locked);
 
 // Network Settings
-pref("network.automatic-ntlm-auth.allow-proxies", false);
-pref("network.auth.sspi_detect_hash", false);
-pref("network.connectivity-service.enabled", false);
-pref("network.dns.disableIPv6", true);
-pref("network.dns.disablePrefetch", true);
-pref("network.early-hints.enabled", false);
-pref("network.fetchpriority.enabled", false);
+pref("network.automatic-ntlm-auth.allow-proxies", false, locked);
+pref("network.auth.sspi_detect_hash", false, locked);
+pref("network.connectivity-service.enabled", false, locked);
+pref("network.dns.disableIPv6", true, locked);
+pref("network.dns.disablePrefetch", true, locked);
+pref("network.early-hints.enabled", false, locked);
+pref("network.fetchpriority.enabled", false, locked);
 
 // UI Settings
-pref("ui.new-webcompat-reporter.enabled", false);
+pref("ui.new-webcompat-reporter.enabled", false, locked);
 
 // Signon Settings
-pref("signon.rememberSignons", false);
+pref("signon.rememberSignons", false, locked);
 
 // Disable WebRTC to prevent IP leak
-pref("media.peerconnection.enabled", false);
+pref("media.peerconnection.enabled", false, locked);
 
 // Disable WASM (web assembly)
-pref("javascript.options.wasm", false);
-pref("devtools.debugger.features.wasm", false);
+pref("javascript.options.wasm", false, locked);
+pref("devtools.debugger.features.wasm", false, locked);
 
 // Enhanced privacy settings
-pref("privacy.trackingprotection.enabled", true);
-pref("privacy.trackingprotection.socialtracking.enabled", true);
+pref("privacy.trackingprotection.enabled", true, locked);
+pref("privacy.trackingprotection.socialtracking.enabled", true, locked);
 
 // Disable geolocation
-pref("geo.enabled", false);
+pref("geo.enabled", false, locked);
 
 // Disable Network Proxy
-pref("network.proxy.type", 0);
+pref("network.proxy.type", 0, locked);
 
 // Disable Autoplay
-pref("media.autoplay.default", 5);
+pref("media.autoplay.default", 5, locked);
