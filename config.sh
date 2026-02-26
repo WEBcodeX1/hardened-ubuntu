9a53fe97538ac55d7ea4f35e29aa54d40c96e9a8
Claus Prüfer <pruefer@webcodex.de>
2026-02-26 11:32:12 +0100

    Add grub extra kernel command line parameters

--- config.sh
+++ config.sh
@@ -13,6 +13,9 @@ export NET_WIFI_AUTH_PASS="Wifi-Password"
 # chronyd
 export NET_NTP_STATIC_SERVER="192.168.1.1"
 
+# grub cmdline
+export GRUB_KERNEL_CMDLINE="i915.enable_dc=0 i915.modeset=1 i915.enable_psr=0 intel_idle.max_cstate=1"
+
 # nextdns
 export NEXTDNS_ID="a1b2c3"
 export NEXTDNS_STAMP="Base64Hash"
