d5bf93f0c5aa5a63cf107fe89df8b6f9a3e06b06
Claus Prüfer <pruefer@webcodex.de>
2026-02-27 18:21:14 +0100

    Fix quotes

--- set-grub-kernel-cmdline.sh
+++ set-grub-kernel-cmdline.sh
@@ -4,7 +4,7 @@
 grub_kernel_cmdline=`printenv GRUB_KERNEL_CMDLINE`
 
 # disable ipv6, set kernel commandline from config
-sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="quiet splash"/GRUB_CMDLINE_LINUX_DEFAULT="quiet splash ipv6.disable=1 ${grub_kernel_cmdline}"/g' /etc/default/grub
+sed -i "s/GRUB_CMDLINE_LINUX_DEFAULT=\"quiet splash\"/GRUB_CMDLINE_LINUX_DEFAULT=\"quiet splash ipv6.disable=1 ${grub_kernel_cmdline}\"/g" /etc/default/grub
 
 # disable crashkernel
 echo 'GRUB_CMDLINE_LINUX_DEFAULT="$GRUB_CMDLINE_LINUX_DEFAULT crashkernel=no"' > /etc/default/grub.d/kdump-tools.cfg
